c293234a359df9e4d92b3a7c9a7fb47455d33fcd
bgamwell <bgamwell@gmail.com>
2015-09-02 17:47:48 -0700

    Updated readme

---
+++ README.md
@@ -0,0 +1,14 @@
+#User Story#
+
+LGBT bar-goers don't have many options when it comes to locating LGBT-friendly venues--in fact, most people rely on print publications, or friends (or friends of friends) in San Francisco. But what if a user could pull out his or her phone and visualize where the nearest gay bars are, displaying the most up-to-date venue information from a trusted, publicly accessible API like Foursquare.
+
+Last Call is a simple, one-page application, mobile/hybrid application that does ONE thing really well: It helps users find bars.
+
+#Here's what a user's experience would be like using the app:#
+
+ - Users first see a map of San Francisco auto-populated with all of Ssan Francisco's LGBT bars as pins.
+ - Upon tap, each pin will display an info window with location name, address and a link to the bar's external website.
+ - A user will have the option to save a venue in his or her repertoire of favorites. Maps will not be saved; rather, a user will be saving bar information, which will then be rendered to his/her profile view.
+ - A user will have the option to draw a route between his/her current location and a venue. This command should appear in an expanded info window.
+ - A user can view his/her profile by clicking a button on the top-left of the main screen. A hidden div will slide out from the left. If a user has already signed up and signed in, their profile will display, along with a list of favorite bars. On click, the map will zoom to that particular bar. (Stretch: A route between the user's location and that bar will be rendered to the map.)
+ - Note: Users can always use the app whether or not they are logged in, but only signed-in users can save favorites to their profile.
--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
   }]);
 
 // Drop many pins at once
-app.controller('BarPinsCtrl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
+app.controller('BarPinsCtrl', ['$rootScope', '$scope', '$timeout', '$http', function($rootScope, $scope, $timeout, $http) {
   $scope.image = {
     url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
     size: [20, 32],
@@ -34,43 +34,43 @@ app.controller('BarPinsCtrl', ['$scope', '$timeout', '$http', function($scope, $
     $scope.map.setZoom(16);
   };
 
-  $http.get('https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d1d8941735&near=San_Francisco,CA&limit=50&client_id=F0C3XEK1WU1ONFBX5DZPE3FK3XK224Q2VV0HRKFY2UFAY45Q&client_secret=LWOZYT1HB4555JUYZIJBTVDNNUL3ZY2SZCCE5FMIV2TRP0CN&v=20150806')
-        .then(function (response) {
-          console.log(response.data.response.venues);
-          $scope.bars = response.data.response.venues;
-        });
+  // $http.get('https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d1d8941735&near=San_Francisco,CA&limit=50&client_id=F0C3XEK1WU1ONFBX5DZPE3FK3XK224Q2VV0HRKFY2UFAY45Q&client_secret=LWOZYT1HB4555JUYZIJBTVDNNUL3ZY2SZCCE5FMIV2TRP0CN&v=20150806')
+  //       .then(function (response) {
+  //         console.log(response.data.response.venues);
+  //         $rootScope.bars = response.data.response.venues;
+  //       });
 
-  // $scope.bars = [
-  //   {'id': 1, name: 'Powerhouse', lat: 37.7730761, lng: -122.4121004, description: ''},
-  //   {'id': 2, name: 'Castro Theatre', lat: 37.7620377, lng: -122.4346968, description: ''},
-  //   {'id': 3, name: 'Badlands', lat: 37.7607011, lng: -122.4354564, description: ''},
-  //   {'id': 4, name: '440 Castro', lat: 37.7618428, lng: -122.4353514, description: ''},
-  //   {'id': 5, name: 'The Cafe', lat: 37.7627891, lng: -122.4343241, description: ''},
-  //   {'id': 6, name: 'Q Bar', lat: 37.7616213, lng: -122.4352878, description: ''},
-  //   {'id': 7, name: 'Moby Dick', lat: 37.7608172, lng: -122.4336979, description: ''},
-  //   {'id': 8, name: 'The Edge', lat: 37.7608172, lng: -122.4336979, description: ''},
-  //   {'id': 9, name: 'Midnight Sun', lat: 37.7607688, lng: -122.482758, description: ''},
-  //   {'id': 10, name: 'Lookout', lat: 37.764383, lng: -122.4334021, description: ''},
-  //   {'id': 11, name: 'Lone Star Saloon', lat: 37.7721444, lng: -122.4108652, description: ''},
-  //   {'id': 12, name: 'The Mix', lat: 37.7610948, lng: -122.4345455, description: ''},
-  //   {'id': 13, name: 'Toad Hall', lat: 37.7609714, lng: -122.4357522, description: ''},
-  //   {'id': 14, name: 'Twin Peaks Tavern', lat: 37.7623731, lng: -122.4349194, description: ''},
-  //   {'id': 15, name: 'El Rio', lat: 37.7467935, lng: -122.4194204, description: ''},
-  //   {'id': 16, name: 'Pilsner Inn', lat: 37.7671169, lng: -122.4287035, description: ''},
-  //   {'id': 17, name: 'The Stud', lat: 37.7728278, lng: -122.410002, description: ''},
-  //   {'id': 18, name: 'SF Eagle', lat: 37.7700044, lng: -122.4133666, description: ''},
-  //   {'id': 19, name: 'Hole in the Wall Saloon', lat: 37.7729582, lng: -122.4123879, description: ''},
-  //   {'id': 20, name: 'Underground SF', lat: 37.7724118, lng: -122.4293812, description: ''},
-  //   // {'id': 21, name: "Aunt Charlie's Lounge", lat: 37.7829004, lng: -122.4113768, description: ''},
-  //   {'id': 22, name: 'The Cinch Saloon', lat: 37.7927352, lng: -122.4215355, description: ''},
-  //   {'id': 23, name: 'Hi Tops', lat: 37.7649285, lng: -122.4317197, description: ''},
-  //   {'id': 24, name: 'Trax', lat: 37.7700047, lng: -122.4460374, description: ''},
-  //   {'id': 25, name: 'Beaux', lat: 37.7700047, lng: -122.4460374, description: ''},
-  //   {'id': 26, name: 'BeatBox', lat: 37.7714872, lng: -122.413929, description: ''},
-  //   {'id': 27, name: 'Last Call', lat: 37.7714872, lng: -122.413929, description: ''},
-  //   {'id': 28, name: 'Gangway', lat: 37.7858289, lng: -122.4183209, description: ''},
-  //   {'id': 29, name: 'OMG!', lat: 37.7858289, lng: -122.4183209, description: ''},
-  //   {'id': 30, name: 'Oasis', lat: 37.7746696, lng: -122.4166472, description: ''}
-  // ];
+  $scope.bars = [
+    {'id': 1, name: 'Powerhouse', lat: 37.7730761, lng: -122.4121004, description: ''},
+    {'id': 2, name: 'Castro Theatre', lat: 37.7620377, lng: -122.4346968, description: ''},
+    {'id': 3, name: 'Badlands', lat: 37.7607011, lng: -122.4354564, description: ''},
+    {'id': 4, name: '440 Castro', lat: 37.7618428, lng: -122.4353514, description: ''},
+    {'id': 5, name: 'The Cafe', lat: 37.7627891, lng: -122.4343241, description: ''},
+    {'id': 6, name: 'Q Bar', lat: 37.7616213, lng: -122.4352878, description: ''},
+    {'id': 7, name: 'Moby Dick', lat: 37.7608172, lng: -122.4336979, description: ''},
+    {'id': 8, name: 'The Edge', lat: 37.7608172, lng: -122.4336979, description: ''},
+    {'id': 9, name: 'Midnight Sun', lat: 37.7607688, lng: -122.482758, description: ''},
+    {'id': 10, name: 'Lookout', lat: 37.764383, lng: -122.4334021, description: ''},
+    {'id': 11, name: 'Lone Star Saloon', lat: 37.7721444, lng: -122.4108652, description: ''},
+    {'id': 12, name: 'The Mix', lat: 37.7610948, lng: -122.4345455, description: ''},
+    {'id': 13, name: 'Toad Hall', lat: 37.7609714, lng: -122.4357522, description: ''},
+    {'id': 14, name: 'Twin Peaks Tavern', lat: 37.7623731, lng: -122.4349194, description: ''},
+    {'id': 15, name: 'El Rio', lat: 37.7467935, lng: -122.4194204, description: ''},
+    {'id': 16, name: 'Pilsner Inn', lat: 37.7671169, lng: -122.4287035, description: ''},
+    {'id': 17, name: 'The Stud', lat: 37.7728278, lng: -122.410002, description: ''},
+    {'id': 18, name: 'SF Eagle', lat: 37.7700044, lng: -122.4133666, description: ''},
+    {'id': 19, name: 'Hole in the Wall Saloon', lat: 37.7729582, lng: -122.4123879, description: ''},
+    {'id': 20, name: 'Underground SF', lat: 37.7724118, lng: -122.4293812, description: ''},
+    // {'id': 21, name: "Aunt Charlie's Lounge", lat: 37.7829004, lng: -122.4113768, description: ''},
+    {'id': 22, name: 'The Cinch Saloon', lat: 37.7927352, lng: -122.4215355, description: ''},
+    {'id': 23, name: 'Hi Tops', lat: 37.7649285, lng: -122.4317197, description: ''},
+    {'id': 24, name: 'Trax', lat: 37.7700047, lng: -122.4460374, description: ''},
+    {'id': 25, name: 'Beaux', lat: 37.7700047, lng: -122.4460374, description: ''},
+    {'id': 26, name: 'BeatBox', lat: 37.7714872, lng: -122.413929, description: ''},
+    {'id': 27, name: 'Last Call', lat: 37.7714872, lng: -122.413929, description: ''},
+    {'id': 28, name: 'Gangway', lat: 37.7858289, lng: -122.4183209, description: ''},
+    {'id': 29, name: 'OMG!', lat: 37.7858289, lng: -122.4183209, description: ''},
+    {'id': 30, name: 'Oasis', lat: 37.7746696, lng: -122.4166472, description: ''}
+  ];
 
 }]);
